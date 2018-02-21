a2fa477c8d0a62370d6c130dc603cc824168a507
Giles Miclotte <giles.miclotte@ugent.be>
2018-02-21 13:57:05 +0100

    Add brownie and karect as submodules

--- compile.sh
+++ compile.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-rm src/*~
 mkdir -p build
 cd build
-	cmake ../
-	make
+	cmake ..
+	make -j
 cd ..
 mkdir -p bin
-cp -b ./build/src/jabba ./bin/jabba
+cp ./build/src/jabba ./bin/jabba
 #cd src
 #doxygen ../Profiling/Doxyfile > ../Profiling/doxygen_output
 #pmccabe -vt *.hpp *.cpp > ../Profiling/pmccabe_output
+
+git submodule update --init
+cd brownie
+	mkdir -p build
+	cd build
+		cmake -DMAXKMERLENGTH=75 ..
+		make -j
+	cd ..
+cd ..
+cp ./brownie/build/src/brownie ./bin/brownie
+
+cd karect
+	make -j
+cd ..
+cp ./karect/karect ./bin/karect
+
